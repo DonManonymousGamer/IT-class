eeed6c0e77d201b063b65c2617d2d431c9deb18c
DonManonymousGamer <151723415+DonManonymousGamer@users.noreply.github.com>
2023-11-22 17:04:52 +0100

    Update Würfelspiel.py
    final version

--- Würfelspiel.py
+++ Würfelspiel.py
@@ -2,23 +2,43 @@ import random
 from gturtle import *
 makeTurtle()
 ht()
-def wurfeln():
-    return random.randint(1, 6)
-#def wuerfchen():
-    #print("idk")
+screen = Screen()
+
+wurf_summe, differenz, zufallszahl, count = 0, 0, 0, 0
+
+def idk_random():
+    global wurf_summe
+    global count
+    
+    wurf = random.randint(1, 6)
+    wurf_summe += wurf
+    count = count + 1
+    print(count, "Wurf:", wurf)
+    print("Drücken Sie die Eingabetaste für die nächste.")
+    
 def spiel():
+    global differenz
+    global wurf_summe
+    global zufallszahl
+    global count
+    
     zufallszahl = random.randint(20, 40)
+    print("Zielnummer ist:", zufallszahl)
     anzahl_wurfel = int(input("Wie oft möchtest du würfeln? "))
-    wurf_summe = 0
-    count = 0
-    for _ in range(anzahl_wurfel):
-        input("Drücke eine beliebige Taste, um zu würfeln.")
-        wurf = wurfeln()
-        wurf_summe += wurf
-        count = count + 1
-        print(count, "Wurf:", wurf)
-    differenz = abs(wurf_summe - zufallszahl)
-    print("Die Summe der Würfelwürfe beträgt:", wurf_summe)
-    print("Die Differenz zur Zufallszahl", zufallszahl, "beträgt:", differenz)
+
+
+    screen.onkey(idk_random, "enter")
+    screen.listen()
+    
+    while True:
+        if (anzahl_wurfel <= count):
+            differenz = abs(wurf_summe - zufallszahl)
+            print("Die Summe deiner Würfe war:", wurf_summe)
+            print("Ihre Differenz zu", zufallszahl, "beträgt:", differenz)
+            break
+            
+    screen.bye()
+    
 
 spiel()
+
