97492a157b17b1922b9eb3a99cda02fcade44178
DonManonymousGamer <151723415+DonManonymousGamer@users.noreply.github.com>
2023-11-22 09:26:11 +0100

    1. Version des Würfelspiels

---
+++ Würfelspiel.py
@@ -0,0 +1,17 @@
+﻿import random
+from gturtle import *
+makeTurtle()
+ht()
+print("Le Würfelspiel")
+Zielnummer=(random.randrange(20, 40))
+print(Zielnummer)
+wuerfeln = input("Welches Key willst du zum Würfeln benutzen?")
+Frage = inputInt("Wie oft willst du würfeln?")
+zahl=0
+def wuerfchen():
+    zahl=zahl+1
+    print(zahl, "Wurf ist:")
+    
+
+Screen().onkey(wuerfeln, wuerfchen)
+Screen().listen()
